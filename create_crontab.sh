048c01fb4c125472b12d246ead73a506ea099e5a
Jason Kridner <github@hangerhead.com>
2014-06-30 13:30:23 -0400

    Update create_crontab.sh

--- create_crontab.sh
+++ create_crontab.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-echo "*/5 6,7,8,9,10,11,12,13,14,15,16,17,18,19,20 * * * python /var/lib/cloud9/Dish­Detector/test-­sink-­and-­email.py" | crontab -
+echo "*/5 6,7,8,9,10,11,12,13,14,15,16,17,18,19,20 * * * python /var/lib/cloud9/Dish-­Detector/test-­sink-­and-­email.py" | crontab -
